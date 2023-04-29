4c820ab34171c438b5a2f7498c9f5e2bcf2f549d
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-29 11:41:45 -0600

    First Commit

---
+++ src/main/java/com/example/springjpaadvance/SpringJpaAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpaadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaAdvanceApplication.class, args);
+	}
+
+}
