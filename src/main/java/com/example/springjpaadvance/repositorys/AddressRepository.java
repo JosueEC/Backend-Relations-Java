3abcaa9d5a23ed86e04f9e4a7d6f826aee84fd4d
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-29 14:12:08 -0600

    fix(repositorys): Agregamos la etiqueta @Repository
    No estaba agregada la etiqueta @Repository a la interface asi que la agregamos

--- src/main/java/com/example/springjpaadvance/repositorys/AddressRepository.java
+++ src/main/java/com/example/springjpaadvance/repositorys/AddressRepository.java
@@ -2,6 +2,8 @@ package com.example.springjpaadvance.repositorys;
 
 import com.example.springjpaadvance.domain.Address;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends CrudRepository<Address,Long> {
 }
