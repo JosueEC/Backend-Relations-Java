97ddc2a6b44b6ecb51428fc33fcfcba4b129206c
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-30 10:16:20 -0600

    docs(src): Probamos la funcionalidad de la relacion @OneToMany
    Agregamos codigo a la funcion main de nuestra aplicacion con Spring-boot para probar la funcionalidad de la relacion @OneToMany, creamos varias entidades Address y se las asignamos a una entidad Client, despues mostramos los datos del registro en consola

--- src/main/java/com/example/springjpaadvance/SpringJpaAdvanceApplication.java
+++ src/main/java/com/example/springjpaadvance/SpringJpaAdvanceApplication.java
@@ -1,13 +1,17 @@
 package com.example.springjpaadvance;
 
+import com.example.springjpaadvance.domain.Address;
 import com.example.springjpaadvance.domain.Client;
 import com.example.springjpaadvance.domain.User;
+import com.example.springjpaadvance.repositorys.AddressRepository;
 import com.example.springjpaadvance.repositorys.ClientRepository;
 import com.example.springjpaadvance.repositorys.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringJpaAdvanceApplication {
@@ -17,12 +21,25 @@ public class SpringJpaAdvanceApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner (UserRepository userRepository, ClientRepository clientRepository) {
+	CommandLineRunner commandLineRunner (UserRepository userRepository, ClientRepository clientRepository, AddressRepository addressRepository) {
 		return args -> {
 			User user1 = userRepository.save(new User(null,"josue-cruz","123"));
-			Client client1 = clientRepository.save(new Client(null,"Josue Evangelista Cruz", user1));
-			System.out.println(client1.toString());
+			Client client1 = clientRepository.save(new Client(null,"Josue Evangelista Cruz", user1, null));
+			Address address1 = addressRepository.save(new Address(null, "Negrete", "15", client1));
+			Address address2 = addressRepository.save(new Address(null, "Allende", "10", client1));
+			Address address3 = addressRepository.save(new Address(null, "Morelos", "5", client1));
+			Address address4 = addressRepository.save(new Address(null, "Iturbe", "2", client1));
+			client1.setAddresses(Set.of(address1, address2, address3, address4));
+			Client josue = clientRepository.save(client1);
+			System.out.println("Client name: " + josue.getName());
+			System.out.println("User name: " + josue.getUser().getUsername());
+			josue.getAddresses().forEach(address -> {
+				System.out.println("Address: " + address.getStreet() + " No." + address.getNumber() );
+			});
+			System.out.println(josue);
 		};
 	}
-
 }
+
+// Nota: Cuando hacemos un save de una entidad que ya antes fue creada, osea que ya tiene un id asignado, JPA
+// interpreta que se trata de un UPDATE y no una creacion como tal, debido a que el registro ya existe
