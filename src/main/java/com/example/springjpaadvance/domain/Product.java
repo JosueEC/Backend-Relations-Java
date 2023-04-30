fed2506ba8a10846301dfc68dcb11f640d7f9538
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-30 10:24:50 -0600

    feat(domain): Creacion de la entidad Produtc
    Creamos la entidad Product para poner en practica la relacion @ManyToMany

---
+++ src/main/java/com/example/springjpaadvance/domain/Product.java
@@ -0,0 +1,30 @@
+package com.example.springjpaadvance.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @SequenceGenerator(
+            name = "product_sequence",
+            sequenceName = "product_sequence",
+            allocationSize = 3,
+            initialValue = 150
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_sequence"
+    )
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private Double price;
+}
