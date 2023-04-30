dd94dfeb14451c234cf8c158044e927aa6437da5
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-04-30 11:06:41 -0600

    feat(src): Probamos la relacion @ManyToMany entre Client y Products
    Probamos rapidamente en la funcion main la forma de crear relaciones @ManyToMany, creamos varios productos y los asignamos a un Cliente

--- src/main/java/com/example/springjpaadvance/SpringJpaAdvanceApplication.java
+++ src/main/java/com/example/springjpaadvance/SpringJpaAdvanceApplication.java
@@ -2,9 +2,11 @@ package com.example.springjpaadvance;
 
 import com.example.springjpaadvance.domain.Address;
 import com.example.springjpaadvance.domain.Client;
+import com.example.springjpaadvance.domain.Product;
 import com.example.springjpaadvance.domain.User;
 import com.example.springjpaadvance.repositorys.AddressRepository;
 import com.example.springjpaadvance.repositorys.ClientRepository;
+import com.example.springjpaadvance.repositorys.ProductRepository;
 import com.example.springjpaadvance.repositorys.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,22 +23,42 @@ public class SpringJpaAdvanceApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner (UserRepository userRepository, ClientRepository clientRepository, AddressRepository addressRepository) {
+	CommandLineRunner commandLineRunner (UserRepository userRepository, ClientRepository clientRepository, AddressRepository addressRepository, ProductRepository productRepository) {
 		return args -> {
+			// Client and User
 			User user1 = userRepository.save(new User(null,"josue-cruz","123"));
+
+			// Asignacion @OneToOne
 			Client client1 = clientRepository.save(new Client(null,"Josue Evangelista Cruz", user1, null, null));
+
+			// Address
 			Address address1 = addressRepository.save(new Address(null, "Negrete", "15", client1));
 			Address address2 = addressRepository.save(new Address(null, "Allende", "10", client1));
 			Address address3 = addressRepository.save(new Address(null, "Morelos", "5", client1));
 			Address address4 = addressRepository.save(new Address(null, "Iturbe", "2", client1));
+
+			// Products
+			Product product1 = productRepository.save(new Product(null, "Teclado Asus", 200.00));
+			Product product2 = productRepository.save(new Product(null, "Mouse Logitech", 450.99));
+			Product product3 = productRepository.save(new Product(null, "Monitor Acer", 1200.50));
+			Product product4 = productRepository.save(new Product(null, "Escritorio elevable", 9000.00));
+
+			// Asignacion @OneToMany
 			client1.setAddresses(Set.of(address1, address2, address3, address4));
+			// Asignacion @ManyToMany
+			client1.setProducts(Set.of(product1, product2, product3));
+
 			Client josue = clientRepository.save(client1);
 			System.out.println("Client name: " + josue.getName());
 			System.out.println("User name: " + josue.getUser().getUsername());
 			josue.getAddresses().forEach(address -> {
 				System.out.println("Address: " + address.getStreet() + " No." + address.getNumber() );
 			});
-			System.out.println(josue);
+			System.out.println("-----------------------------------------------------------------------");
+			josue.getProducts().forEach(product -> {
+				System.out.println("Product Description: " + product.getName() + " $" + product.getPrice());
+			});
+ 			System.out.println(josue);
 		};
 	}
 }
